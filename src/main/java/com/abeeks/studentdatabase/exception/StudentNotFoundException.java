72f5715f77ba9e59aff26cb1ae6abed2283dd902
Student10 <student10@Codes-MacBook-Air-85.local>
2024-02-07 10:10:35 -0500

    more comments added

--- src/main/java/com/abeeks/studentdatabase/exception/StudentNotFoundException.java
+++ src/main/java/com/abeeks/studentdatabase/exception/StudentNotFoundException.java
@@ -2,6 +2,7 @@ package com.abeeks.studentdatabase.exception;
 
 public class StudentNotFoundException extends RuntimeException {
     public StudentNotFoundException(String message) {
+        // Call the constructor of the superclass (RuntimeException) with the provided message
         super(message);
     }
 }
